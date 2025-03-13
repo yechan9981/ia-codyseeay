0e00706ea84ae5a76a8f4218c2570eed52a9f6f2
Chuntoo <127668815+yechan9981@users.noreply.github.com>
2025-03-13 19:16:41 +0900

    update

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def read_log_file(filename):
     except Exception as e:
         print(f"에러 발생: {e}")
 
-if __name__ == "__main__":
-    log_file = "mission_computer_main.log"
-    read_log_file(log_file)
+
+log_file = "mission_computer_main.log"
+read_log_file(log_file)
