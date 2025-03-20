950d70c83a97f984cd361fd3a3e88a1c5e2e2fec
Chuntoo <127668815+yechan9981@users.noreply.github.com>
2025-03-20 19:31:10 +0900

    update
    update

--- 2week/listLog.py
+++ 2week/listLog.py
@@ -38,11 +38,6 @@ except ValueError:
     print('오류: "Flammability" 값을 숫자로 변환할 수 없습니다.')
     exit(1)
 
-# 정렬된 화물 목록 출력
-print('\n인화성 높은 순으로 정렬된 화물 목록:')
-for item in sorted_inventory:
-    print(item)
-
 # 인화성 지수 0.7 이상인 항목 추출
 dangerous_items = [
     item for item in sorted_inventory if float(item['Flammability']) >= 0.7
@@ -73,7 +68,7 @@ except Exception as e:
 
 # 정렬된 배열을 이진 파일로 저장
 try:
-    with open('2week/Mars_Base_Inventory_List.bin', 'wb') as file:
+    with open('2week/result/Mars_Base_Inventory_List.bin', 'wb') as file:
         for item in sorted_inventory:
             # 딕셔너리를 문자열로 변환 후 바이트로 인코딩
             line = f"{item['Substance']},{item['Weight (g/cm³)']},{item['Specific Gravity']},{item['Strength']},{item['Flammability']}\n"
@@ -86,7 +81,7 @@ except Exception as e:
 # 이진 파일 읽기 및 출력
 print('\n"Mars_Base_Inventory_List.bin"에서 읽어온 내용:')
 try:
-    with open('2week/Mars_Base_Inventory_List.bin', 'rb') as file:
+    with open('2week/result/Mars_Base_Inventory_List.bin', 'rb') as file:
         binary_content = file.read()
         # 바이트를 문자열로 디코딩하여 출력
         decoded_content = binary_content.decode('utf-8')
